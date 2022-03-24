d59b8540c3e7a9d247dd7420830cc1bd1cb71c48
Maxime Point <maximepoint6@gmail.com>
2022-03-24 14:35:37 +0100

    Initial Commit

---
+++ ApolloGraphQL/ApolloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGraphQLApp.swift
+//  ApolloGraphQL
+//
+//  Created by Maxime Point on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
